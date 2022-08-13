99e06fef2427f104c588964e809e380c63aef457
Gabriel Agustin Uriarte <gabri02.uri@gmail.com>
2022-08-13 10:46:39 +0200

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,12 @@
-import { Table } from './components/Table';
-
+import { MainPage } from './components/MainPage';
 import './App.css';
-
-import { products } from './data';
-import { useState } from 'react';
-import { Product } from './models/Product';
-import { MapperTable } from './components/TableTypes';
 
 function App() {
 
-	const [items, setItems] = useState<Product[]>(products);
-
-	const mapperTable: MapperTable<Product>[] = [
-		{ 
-			label: 'Name', accessor: 'name', 
-			sortable: true, cellClassName: 'table__cell table__cell--text-left',
-			headerClassName: 'table__heading'
-		},
-		{ 
-			label: 'Price', accessor: 'price', 
-			sortable: true, cellClassName: 'table__cell table__cell--text-centered',
-			headerClassName: 'table__heading'
-		}
-	]
 
 	return (
 		<div className="app">
-			<main className='container'>
-				<Table
-					items={items}  
-					setItems={setItems}
-					mapperElements={mapperTable}
-					caption={'List of products'}
-				/>
-			</main>
+			<MainPage/>
 		</div>
 	);
 }
